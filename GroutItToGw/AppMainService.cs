6c3e54e5123b80a61a99c977b8e9359794881ae3
chrumck <chrumck@gmail.com>
2015-08-12 15:29:34 -0700

    20150812-1529

---
+++ GroutItToGw/AppMainService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GroutItToGw
+{
+    class AppMainService
+    {
+        //Fields and Properties------------------------------------------------------------------------------------------------//
+
+        private AppSettings appSettings;
+
+        //Constructors---------------------------------------------------------------------------------------------------------//
+        public AppMainService(AppSettings appSettings)
+        {
+            this.appSettings = appSettings;
+        }
+
+        //Methods--------------------------------------------------------------------------------------------------------------//
+
+
+
+
+        //Helpers--------------------------------------------------------------------------------------------------------------//
+        #region Helpers
+
+        #endregion
+                
+    }
+}
+
---
+++ GroutItToGw/FormMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GroutItToGw
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.LblErrCount = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TxBStatus = new System.Windows.Forms.TextBox();
+            this.BtnSettings = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(12, 12);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(109, 38);
+            this.BtnStart.TabIndex = 12;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStop.Location = new System.Drawing.Point(12, 56);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(109, 38);
+            this.BtnStop.TabIndex = 13;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            // 
+            // LblErrCount
+            // 
+            this.LblErrCount.AutoSize = true;
+            this.LblErrCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblErrCount.Location = new System.Drawing.Point(160, 186);
+            this.LblErrCount.Name = "LblErrCount";
+            this.LblErrCount.Size = new System.Drawing.Size(14, 13);
+            this.LblErrCount.TabIndex = 19;
+            this.LblErrCount.Text = "0";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(8, 186);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(146, 13);
+            this.Label2.TabIndex = 18;
+            this.Label2.Text = "Errors Count Since Last Start:";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(11, 132);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(78, 13);
+            this.Label1.TabIndex = 17;
+            this.Label1.Text = "Last Log Entry:";
+            // 
+            // TxBStatus
+            // 
+            this.TxBStatus.Location = new System.Drawing.Point(11, 154);
+            this.TxBStatus.Name = "TxBStatus";
+            this.TxBStatus.Size = new System.Drawing.Size(360, 20);
+            this.TxBStatus.TabIndex = 16;
+            // 
+            // BtnSettings
+            // 
+            this.BtnSettings.Location = new System.Drawing.Point(282, 12);
+            this.BtnSettings.Name = "BtnSettings";
+            this.BtnSettings.Size = new System.Drawing.Size(89, 25);
+            this.BtnSettings.TabIndex = 15;
+            this.BtnSettings.Text = "Settings";
+            this.BtnSettings.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(299, 180);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(72, 25);
+            this.BtnClose.TabIndex = 14;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(127, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 211);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LblErrCount);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TxBStatus);
+            this.Controls.Add(this.BtnSettings);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "GroutItToGw";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button BtnStart;
+        internal System.Windows.Forms.Button BtnStop;
+        internal System.Windows.Forms.Label LblErrCount;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox TxBStatus;
+        internal System.Windows.Forms.Button BtnSettings;
+        internal System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
